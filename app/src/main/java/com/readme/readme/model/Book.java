e4946bdcd90ce12683a98c111568b422e63bfd29
felipecassiors <felipe.santos@gec.inatel.br>
2018-05-17 21:41:52 -0300

    #esperança

---
+++ app/src/main/java/com/readme/readme/model/Book.java
@@ -0,0 +1,9 @@
+package com.readme.readme.model;
+
+public class Book {
+    private String Name;
+    private String Author;
+    private int totalPages;
+    private int actualPage;
+
+}
---
+++ app/src/main/java/com/readme/readme/model/User.java
@@ -0,0 +1,7 @@
+package com.readme.readme.model;
+
+import java.util.ArrayList;
+
+public class User {
+    private ArrayList<Book> books;
+}
